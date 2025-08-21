a188a75eca943551de1221613d34bd7d135d6137
Yumi Yukimura <me.cafebabe@gmail.com>
2025-08-21 21:16:13 +0800

    Revert "mithorium-common: Disable UFFD GC via `OVERRIDE_ENABLE_UFFD_GC`"
    This reverts commit d50a58a0203786b8861eb715c8e180a12913efba.
    
    Change-Id: I44ace21c879d36eaa44bb9b162d72f75b624920f

--- mithorium.mk
+++ mithorium.mk
@@ -27,9 +27,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG ?= xhdpi
 
-# UFFD GC
-OVERRIDE_ENABLE_UFFD_GC := false
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
